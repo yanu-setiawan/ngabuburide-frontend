cd45fb79e4a5868cab91c643bdb90fefb20181f9
zikriaulia28 <zikriaulia98@gmail.com>
2023-04-08 14:29:25 +0700

    update product

--- src/Pages/Product/index.jsx
+++ src/Pages/Product/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Card from "./card";
+import arrow from "../../assets/icon-arrow-right.svg";
+import arrowDropdown from "../../assets/icon-dropdown.svg";
 
 function Product() {
   return (
@@ -9,6 +11,11 @@ function Product() {
       <Header />
       <main className="">
         <section className="relative">
+          <div className="absolute flex gap-4 px-20 pt-10">
+            <p>Shop</p>
+            <img src={arrow} alt="icon-arrow" />
+            <p>Shop Right Sidebar</p>
+          </div>
           <div className=" hero-login w-full h-[15.6rem] opacity-20 flex justify-center items-center "></div>
           <div className=" absolute flex justify-center items-center text-center flex-col top-16 w-full ">
             <h1 className=" text-[3.5rem] text-blackSec">Let’s Shopping</h1>
@@ -17,44 +24,44 @@ function Product() {
             </p>
           </div>
         </section>
-        <section className="w-full h-full flex flex-col md:flex-row justify-center gap-12 lg:px-20 lg:py-16 ">
+        <section className="w-full h-full flex flex-col md:flex-row justify-center px-4 py-6 gap-12 lg:px-20 lg:py-16 ">
           <div className="flex flex-col gap-10">
             <div className="flex flex-col gap-4 ">
               <h1 className="font-bold text-2xl mb-4">Categories</h1>
-              <div className="flex flex-col gap-8">
-                <div className="flex justify-between gap-36">
+              <div className="flex flex-col md:gap-2 lg:gap-8">
+                <div className="flex justify-between md:gap-24 gap-36 lg:gap-36">
                   <div>Accessories</div>
                   <div>5</div>
                 </div>
-                <div className="flex justify-between gap-36">
+                <div className="flex justify-between md:gap-24 gap-36 lg:gap-36">
                   <div>Brands</div>
                   <div>15</div>
                 </div>
-                <div className="flex justify-between gap-36">
+                <div className="flex justify-between md:gap-24 gap-36 lg:gap-36">
                   <div>Clothing</div>
                   <div>3</div>
                 </div>
-                <div className="flex justify-between gap-36">
+                <div className="flex justify-between md:gap-24 gap-36 lg:gap-36">
                   <div>Fashion</div>
                   <div>8</div>
                 </div>
-                <div className="flex justify-between gap-36">
+                <div className="flex justify-between md:gap-24 gap-36 lg:gap-36">
                   <div>Furniture</div>
                   <div>9</div>
                 </div>
-                <div className="flex justify-between gap-36">
+                <div className="flex justify-between md:gap-24 gap-36 lg:gap-36">
                   <div>Man</div>
                   <div>6</div>
                 </div>
-                <div className="flex justify-between gap-36">
+                <div className="flex justify-between md:gap-24 gap-36 lg:gap-36">
                   <div>Woman</div>
                   <div>8</div>
                 </div>
-                <div className="flex justify-between gap-36">
+                <div className="flex justify-between md:gap-24 gap-36 lg:gap-36">
                   <div>Shoes</div>
                   <div>11</div>
                 </div>
-                <div className="flex justify-between gap-36">
+                <div className="flex justify-between md:gap-24 gap-36 lg:gap-36">
                   <div>Wallets</div>
                   <div>11</div>
                 </div>
@@ -98,11 +105,23 @@ function Product() {
           <div>
             <div className="flex justify-between mb-10">
               <p>Showing 1-16 of 39 Results</p>
-              <div>
-                <p>Sort by</p>
+              <div className="flex flex-col">
+                <div className="flex items-center gap-4">
+                  <p>Sort by</p>
+                  <div>
+                    <img src={arrowDropdown} alt="dropdown" />
+                  </div>
+                </div>
+                <div className="w-52 h-56 bg-[#1A1A1A] text-white mt-8">
+                  <ul className="grid place-items-center px-10 py-12 gap-8">
+                    <li className="cursor-pointer">Latest Product</li>
+                    <li className="cursor-pointer">More Expensive</li>
+                    <li className="cursor-pointer">More Cheap</li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 place-content-between">
+            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 place-items-center">
               <Card />
               <Card />
               <Card />
@@ -112,7 +131,7 @@ function Product() {
               <Card />
               <Card />
             </div>
-            <div className="mt-8">
+            <div className="mt-8 grid place-items-center md:place-content-start lg:place-content-start">
               <div className="btn-group">
                 <button className="btn btn-sm">1</button>
                 <button className="btn btn-sm btn-active">2</button>
