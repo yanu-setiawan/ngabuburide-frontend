346d9d86b8c00d941b1c85e3b371a02ca0755725
zikriaulia28 <zikriaulia98@gmail.com>
2023-04-09 20:26:20 +0700

    update product

--- src/Pages/Product/card.jsx
+++ src/Pages/Product/card.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import noImg from "../../assets/placehoder-product.png";
 // import product from "../../assets/product.png";
 
 function Card(props) {
   const navigate = useNavigate();
   const imgUrl =
     "https://res.cloudinary.com/dhikerrnk/image/upload/v1680940221/";
-  // console.log(props.img);
+  console.log(props.id, imgUrl + props.img);
   return (
     <>
       <div
@@ -16,7 +17,7 @@ function Card(props) {
       >
         <div className="w-48 h-64 lg:w-[18.3rem] lg:h-[25rem]">
           <img
-            src={imgUrl + props.img}
+            src={props.img !== "" ? imgUrl + props.img : noImg}
             className="w-full h-full object-cover"
           />
         </div>
